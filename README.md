c8fa79ee75a80162de74aa8470290ecb924c736e
wizli595 <abdoowizli@gmail.com>
2023-12-13 10:32:05 +0100

    first route

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 3) cree fichier index.js
 3->1 ) npm i nodemon
 4) node --watch index.js (nodemon index.js)
-5)test app 
+5) test app 
+6) 1ere route
+7) explain route / http methods / request + response
--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-// import { data } from "./data.js";
-
-// data.map((e) => {
-//   console.log(e);
-// });
+// all imports
 import express from "express";
 
+// initial the express app
 const app = express();
+const PORT = 5000;
 
-app.listen(5000, () => {
+app.get("/", (request, response) => {
+  response.send("API IS RUNNING....");
+});
+
+// /:id param
+// /route?id= query
+// body
+
+// start the server in port
+app.listen(PORT, () => {
   console.log("app running on port 5000");
 });
